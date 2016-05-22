0d4497ee8075edfbc6eb41f4a1394e297f55987f
Kamran Ali <kamiali63@gmail.com>
2016-05-23 01:18:20 +0500

    no changes

--- ChatApplication/app/src/main/java/com/example/faiz/mylogin/Contact_Fragment.java
+++ ChatApplication/app/src/main/java/com/example/faiz/mylogin/Contact_Fragment.java
@@ -2,14 +2,11 @@ package com.example.faiz.mylogin;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.annotation.StringRes;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
-import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
@@ -18,11 +15,13 @@ import com.firebase.client.ValueEventListener;
 import java.util.ArrayList;
 
 public class Contact_Fragment extends android.support.v4.app.Fragment {
+    private static final String UUID_KEY = "UUID";
     private ListView listView;
     private ArrayList<EmailAndPass> nameList;
     private ContactListAdapter adapter;
     private Firebase firebase;
     String name;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -42,20 +41,20 @@ public class Contact_Fragment extends android.support.v4.app.Fragment {
             public void onDataChange(DataSnapshot dataSnapshot) {
 
                 for (DataSnapshot data : dataSnapshot.getChildren()) {
-                  EmailAndPass  msg = data.getValue(EmailAndPass.class);
-                   // Log.d("msg:",msg.getU_Id() +" User Id:"+ firebase.getAuth().getUid());
-                       if(msg.getU_Id().equals(firebase.getAuth().getUid())) {
-                         //  Log.d("LOL", msg.getU_Id());
+                    EmailAndPass msg = data.getValue(EmailAndPass.class);
+                    // Log.d("msg:",msg.getU_Id() +" User Id:"+ firebase.getAuth().getUid());
+                    if (msg.getU_Id().equals(firebase.getAuth().getUid())) {
+                        //  Log.d("LOL", msg.getU_Id());
 
-                       }else {
-                         //  Log.d("LOL2","Hahaha");
-                           String image = msg.getImgUrl();
-                           nameList.add(new EmailAndPass(msg.getFname(), msg.getLname(), msg.getEmail(), msg.getPassword(), msg.getDob(), msg.getGender(), msg.getU_Id(), image));
-                           adapter.notifyDataSetChanged();
-                       }
+                    } else {
+                        //  Log.d("LOL2","Hahaha");
+                        String image = msg.getImgUrl();
+                        nameList.add(new EmailAndPass(msg.getFname(), msg.getLname(), msg.getEmail(), msg.getPassword(), msg.getDob(), msg.getGender(), msg.getU_Id(), image));
+                        adapter.notifyDataSetChanged();
+                    }
                 }
-                  //  Log.d("LOL", msg.getU_Id() + " " + firebase.getAuth().getUid());
-                }
+                //  Log.d("LOL", msg.getU_Id() + " " + firebase.getAuth().getUid());
+            }
 
 
             @Override
