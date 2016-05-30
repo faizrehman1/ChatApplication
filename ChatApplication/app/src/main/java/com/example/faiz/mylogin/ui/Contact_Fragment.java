e3c8695b500637c38cdd8c9d6eca927c3032c037
Kamran Ali <kamiali63@gmail.com>
2016-05-31 03:22:32 +0500

    implement FIRE base sDK in contact fragment

--- ChatApplication/app/src/main/java/com/example/faiz/mylogin/ui/Contact_Fragment.java
+++ ChatApplication/app/src/main/java/com/example/faiz/mylogin/ui/Contact_Fragment.java
@@ -11,28 +11,20 @@ import android.widget.ListView;
 import com.example.faiz.mylogin.R;
 import com.example.faiz.mylogin.adaptor.ContactListAdapter;
 import com.example.faiz.mylogin.model.User;
-import com.firebase.client.AuthData;
-import com.firebase.client.DataSnapshot;
-import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
-import com.firebase.client.ValueEventListener;
+import com.example.faiz.mylogin.util.AppLogs;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
 public class Contact_Fragment extends android.support.v4.app.Fragment {
-    private static final String UUID_KEY = "UUID";
     private ListView listView;
     private ArrayList<User> nameList;
     private ContactListAdapter adapter;
-    private Firebase firebase;
-    String name;
-    public FirebaseAuth mAuth;
-    public FirebaseAuth.AuthStateListener mAuthStateListener;
-    private AuthData meData;
+    private FirebaseAuth mAuth;
 
     @Nullable
     @Override
@@ -41,66 +33,54 @@ public class Contact_Fragment extends android.support.v4.app.Fragment {
         super.onCreateView(inflater, container, savedInstanceState);
 
 
-        firebase = new Firebase("https://chatapplicationn.firebaseio.com/");
+        DatabaseReference firebase = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
 
-//        FirebaseDatabase database = FirebaseDatabase.getInstance();
-//        DatabaseReference firebase = database.getReference("");
         listView = (ListView) view.findViewById(R.id.contact_ListView);
-        meData = firebase.getAuth();
         nameList = new ArrayList<>();
         adapter = new ContactListAdapter(getActivity(), nameList);
+
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-       Log.d("id",meData.getUid());
+        Log.d("id", mAuth.getCurrentUser().getUid());
 
         //Getting Single value from fire base and setting it to list View
-        firebase.child("User").addListenerForSingleValueEvent(new ValueEventListener() {
-
+        firebase.child("User").addListenerForSingleValueEvent(new com.google.firebase.database.ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot data : dataSnapshot.getChildren()) {
+            public void onDataChange(com.google.firebase.database.DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
 
-                    User msg = data.getValue(User.class);
-                    Log.d("msg:", msg.getFname());
-                    Log.d("idss",msg.getU_Id());
+                Log.d("user:", user.getFname());
+                Log.d("idss", user.getU_Id());
 
-                         //   + " User Id:" + firebase.getAuth().getUid());
-//                    if (msg.getU_Id().equals(firebase.getAuth().getUid().toString())) {
-//                        //  Log.d("LOL", msg.getU_Id()); yahan error hai na ?
-//                    } else {
-//                        //  Log.d("LOL2","Hahaha");
-//                        String image = msg.getImgUrl();
-//                        nameList.add(new User(msg.getFname(),
-//                                msg.getLname(),
-//                                msg.getEmail(),
-//                                msg.getPassword(),
-//                                msg.getDob(),
-//                                msg.getGender(),
-//                                msg.getU_Id(),
-//                                image));
-//                        adapter.notifyDataSetChanged();
-//                    }
+                if (user.getU_Id().equals(mAuth.getCurrentUser().getUid().toString())) {
+                    Log.d("LOL", user.getU_Id());
+                } else {
+                    String image = user.getImgUrl();
+                    nameList.add(new User(user.getFname(),
+                            user.getLname(),
+                            user.getEmail(),
+                            user.getPassword(),
+                            user.getDob(),
+                            user.getGender(),
+                            user.getU_Id(),
+                            image));
+                    adapter.notifyDataSetChanged();
+
+
                 }
             }
 
-            //  Log.d("LOL", msg.getU_Id() + " " + firebase.getAuth().getUid());
-
-
-            //  Log.d("LOL", msg.getU_Id() + " " + firebase.getAuth().getUid());
-
-
             @Override
-            public void onCancelled(FirebaseError firebaseError) {
-
+            public void onCancelled(DatabaseError databaseError) {
+                AppLogs.loge("database error" + databaseError.getMessage());
             }
+
         });
-
-
-
 
         listView.setAdapter(adapter);
         return view;
 
     }
-
 }
+
 
