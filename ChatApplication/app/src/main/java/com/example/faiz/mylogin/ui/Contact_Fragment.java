7a710a9568a2cab5b7139a68d1cbbe1749f080c6
Kamran Ali <kamiali63@gmail.com>
2016-05-30 15:53:21 +0500

    implement facebook signin

--- ChatApplication/app/src/main/java/com/example/faiz/mylogin/ui/Contact_Fragment.java
+++ ChatApplication/app/src/main/java/com/example/faiz/mylogin/ui/Contact_Fragment.java
@@ -2,20 +2,23 @@ package com.example.faiz.mylogin.ui;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
-
-import com.example.faiz.mylogin.adaptor.ContactListAdapter;
 import com.example.faiz.mylogin.R;
+import com.example.faiz.mylogin.adaptor.ContactListAdapter;
 import com.example.faiz.mylogin.model.User;
-
+import com.firebase.client.AuthData;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
@@ -26,46 +29,56 @@ public class Contact_Fragment extends android.support.v4.app.Fragment {
     private ContactListAdapter adapter;
     private Firebase firebase;
     String name;
+    public FirebaseAuth mAuth;
+    public FirebaseAuth.AuthStateListener mAuthStateListener;
+    private AuthData meData;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.contactview, null);
         super.onCreateView(inflater, container, savedInstanceState);
-        Firebase.setAndroidContext(getActivity().getApplicationContext());
 
-        firebase = new Firebase("https://chatapplicationn.firebaseio.com/");
+
+//        firebase = new Firebase("https://chatapplicationn.firebaseio.com/");
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference firebase = database.getReference("");
         listView = (ListView) view.findViewById(R.id.contact_ListView);
-
+        meData = firebase.getAuth();
         nameList = new ArrayList<>();
         adapter = new ContactListAdapter(getActivity(), nameList);
 
         //Getting Single value from fire base and setting it to list View
-        firebase.child("User").addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+        firebase.child("User").addValueEventListener(new ValueEventListener() {
 
-                for (DataSnapshot data : dataSnapshot.getChildren()) {
+         @Override
+          public void onDataChange(DataSnapshot dataSnapshot) {
+           for (DataSnapshot data : dataSnapshot.getChildren()) {
 
 
+               User msg = data.getValue(User.class);
+               Log.d("msg:", msg.getU_Id() + " User Id:" + firebase.getAuth().getUid());
+//               if (msg.getU_Id().equals(meData.getUid())) {
+//                   //  Log.d("LOL", msg.getU_Id()); yahan error hai na ?
+//               } else {
+//                   //  Log.d("LOL2","Hahaha");
+//                   String image = msg.getImgUrl();
+//                   nameList.add(new User(msg.getFname(),
+//                           msg.getLname(),
+//                           msg.getEmail(),
+//                           msg.getPassword(),
+//                           msg.getDob(),
+//                           msg.getGender(),
+//                           msg.getU_Id(),
+//                           image));
+//                   adapter.notifyDataSetChanged();
+//               }
+           }                                                                  }
 
-                  User msg = data.getValue(User.class);
-                   // Log.d("msg:",msg.getU_Id() +" User Id:"+ firebase.getAuth().getUid());
-                       if(msg.getU_Id().equals(firebase.getAuth().getUid())) {
-                         //  Log.d("LOL", msg.getU_Id());
-
-                       }else {
-                         //  Log.d("LOL2","Hahaha");
-                           String image = msg.getImgUrl();
-                           nameList.add(new User(msg.getFname(), msg.getLname(), msg.getEmail(), msg.getPassword(), msg.getDob(), msg.getGender(), msg.getU_Id(), image));
-                           adapter.notifyDataSetChanged();
-                       }
-                }
-                  //  Log.d("LOL", msg.getU_Id() + " " + firebase.getAuth().getUid());
-
-                }
                 //  Log.d("LOL", msg.getU_Id() + " " + firebase.getAuth().getUid());
 
+
+            //  Log.d("LOL", msg.getU_Id() + " " + firebase.getAuth().getUid());
 
 
             @Override
@@ -78,4 +91,38 @@ public class Contact_Fragment extends android.support.v4.app.Fragment {
         return view;
 
     }
+
+   /* private void fbWithFirebaseAuth() {
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+
+                if (currentUser.getUid().equals(firebase.getAuth().getUid())) {
+                    Log.d("TAG", "onAuthStateChanged:signed_in:" + currentUser.getUid());
+                } else {
+                    nameList.add(new User(currentUser.getDisplayName(), "", currentUser.getEmail(), "", "", "", currentUser.getUid(), currentUser.getPhotoUrl().toString()));
+                    adapter.notifyDataSetChanged();
+
+                    Log.d("TAG", "onAuthStateChanged:signed_Out:");
+                }
+
+            }
+        };*/
 }
+
+   /* @Override
+    public void onStart() {
+        super.onStart();
+//        mAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+      *//*  if (mAuthStateListener != null) {
+            mAuth.removeAuthStateListener(mAuthStateListener);
+        }*//*
+    }*/
+
--- ChatApplication/app/src/main/java/com/example/faiz/mylogin/ui/MainActivity.java
+++ ChatApplication/app/src/main/java/com/example/faiz/mylogin/ui/MainActivity.java
@@ -1,49 +1,99 @@
 package com.example.faiz.mylogin.ui;
 
+
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
 import android.widget.Toast;
 
 import com.example.faiz.mylogin.R;
 import com.example.faiz.mylogin.model.User;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+import org.json.JSONObject;
+
+import java.util.Arrays;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText email, pass, id, password, fname, lname, dob, gender, img_Url;
+    EditText email;
+    EditText pass;
+    EditText id;
+    EditText password;
+    EditText fname;
+    EditText lname;
+    EditText dob;
+    EditText gender;
+    EditText img_Url;
     Firebase firebase;
-    Button buttonSignup, buttonSignin;
-    RadioButton radioButtonMale, radioButtonFemale;
-
+    LoginManager fbLoginMan;
+    Button buttonSignup, buttonSignin, buttonFb;
+    String first_namefire, last_namefire, emailfire, birthdayfire, uuidfire, dpfire, genderfire;
+    private CallbackManager callbackManager;
+    private Profile profile;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Firebase.setAndroidContext(this);
+        fbLoginMan = LoginManager.getInstance();
+        callbackManager = CallbackManager.Factory.create();
+        firebase = new Firebase("https://chatapplicationn.firebaseio.com/");
+        mAuth = FirebaseAuth.getInstance();
+
+        buttonFb = (Button) findViewById(R.id.button_SignIn_CustomFb);
 
         email = (EditText) findViewById(R.id.edt);
         pass = (EditText) findViewById(R.id.edt1);
         buttonSignup = (Button) findViewById(R.id.but);
         buttonSignin = (Button) findViewById(R.id.but1);
 
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+                if (currentUser != null) {
 
-        firebase = new Firebase("https://chatapplicationn.firebaseio.com/");
+                    Log.d("TAG", "onAuthStateChanged:signed_in:" + currentUser.getUid());
 
+
+                } else {
+                    Log.d("TAG", "onAuthStateChanged:signed_out");
+                }
+
+            }
+        };
         buttonSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,6 +104,7 @@ public class MainActivity extends AppCompatActivity {
                 LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
                 View vv = inflater.inflate(R.layout.signup_view, null);
                 id = (EditText) vv.findViewById(R.id.edtviewEmail);
+
                 password = (EditText) vv.findViewById(R.id.edtviewPassword);
                 fname = (EditText) vv.findViewById(R.id.edtviewFirstName);
                 lname = (EditText) vv.findViewById(R.id.edtviewLastName);
@@ -70,7 +121,15 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void onSuccess(Map<String, Object> stringObjectMap) {
 
-                                firebase.child("User").child(stringObjectMap.get("uid").toString()).setValue(new User(fname.getText().toString(), lname.getText().toString(), id.getText().toString(), password.getText().toString(), dob.getText().toString(), gender.getText().toString(), stringObjectMap.get("uid").toString(), img_Url.getText().toString()));
+                                firebase.child("User").child(stringObjectMap.get("uid").toString()).setValue(new
+                                        User(fname.getText().toString(),
+                                        lname.getText().toString(),
+                                        id.getText().toString(),
+                                        password.getText().toString(),
+                                        dob.getText().toString(),
+                                        gender.getText().toString(),
+                                        stringObjectMap.get("uid").toString(),
+                                        img_Url.getText().toString()));
 
 //                            Log.d("Data After Signup",""+stringObjectMap.get("uid"));
                                 Toast.makeText(MainActivity.this, "Successfull", Toast.LENGTH_SHORT).show();
@@ -94,6 +153,124 @@ public class MainActivity extends AppCompatActivity {
                 });
                 builder.setNegativeButton("Cancel", null);
                 builder.show();
+
+            }
+        });
+
+        buttonFb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                fbLoginMan.logInWithReadPermissions(MainActivity.this, Arrays.asList("email",
+                        "user_birthday", "public_profile"));
+
+                fbLoginMan.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        Toast.makeText(MainActivity.this, "LoginSuccessfully Via Facebook", Toast.LENGTH_SHORT).show();
+
+                        AccessToken accessToken = loginResult.getAccessToken();
+
+                        Log.d("accessToken", " " + accessToken);
+
+                        profile = Profile.getCurrentProfile();
+
+                        GraphRequest request = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                try {
+                                    Log.e("Graph Resp Json:", "" + object.toString());
+                                    Log.e("Graph Resp Raw:", "" + response.getRawResponse());
+                                    String token = AccessToken.getCurrentAccessToken().getToken();
+                                    Log.e("Graph Token:", "" + token);
+
+                                    first_namefire = object.getString("first_name");
+                                    Log.e("FirstName", "" + first_namefire);
+
+                                    last_namefire = object.getString("last_name");
+                                    Log.e("LastName", "" + last_namefire);
+
+                                    emailfire = object.getString("email");
+                                    Log.e("Email", "" + emailfire);
+
+                                    birthdayfire = object.getString("birthday");
+                                    Log.e("birthday", "" + birthdayfire);
+
+                                    genderfire = object.getString("gender");
+                                    Log.e("gender :", "" + genderfire);
+
+                                    uuidfire = object.getString("id");
+                                    Log.e("id:", "" + uuidfire);
+
+                                    dpfire = Profile.getCurrentProfile().getProfilePictureUri(400, 400).toString();
+                                    Log.e("Graph Dp:", "" + dpfire);
+
+
+                                    firebase.child("User").child(uuidfire).setValue(new
+                                            User(first_namefire,
+                                            last_namefire,
+                                            emailfire,
+                                            "",
+                                            birthdayfire,
+                                            genderfire,
+                                            uuidfire,
+                                            dpfire
+                                    ));
+
+                                } catch (Exception ex) {
+                                    ex.printStackTrace();
+                                }
+                            }
+
+                        });
+                        Bundle parameters = new Bundle();
+                        String request_params = "id,name,gender,email,birthday";
+                        String old_req_params = "id, first_name, last_name, email,gender, birthday, location";
+                        parameters.putString("fields", old_req_params);
+                        request.setParameters(parameters);
+                        request.executeAsync();
+
+                        /////////////////FB user data save and Sign in END/////////////////////
+                        // user ka jo b data hai wo yaha per get ker k save kerwa k rakh do then jab user firevase se login ho jaye to
+                        //tab wo sab data waha se auth.get id k ref per store kwerwa do
+
+                        //yeh b karlia h
+
+                       /* firebase.child("User").child(uuidfire).setValue(new
+                                User(first_namefire,
+                                last_namefire,
+                                emailfire,
+                                "",
+                                birthdayfire,
+                                genderfire,
+                                uuidfire,
+                                dpfire
+                        ));*/
+
+                        handleFacebookAccessToken(loginResult.getAccessToken());
+
+                        if (profile != null) {
+                            Intent intent = new Intent(MainActivity.this, Navigation_Activity.class);
+                            startActivity(intent);
+                            Log.e("Emailactive", "" + emailfire);
+
+                        } else {
+
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Log.d("TAG", "facebook:onCancel");
+                    }
+
+                    @Override
+                    public void onError(FacebookException error) {
+                        Log.d("TAG", "facebook:onError " + error);
+                    }
+                });
 
             }
         });
@@ -139,9 +316,49 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-//        firebase.child(firebase.getAuth().getUid()).setValue(new EmailAndPass(fname.getText().toString(),lname.getText().toString(),id.getText().toString(),password.getText().toString()));
     }
 
+    private void handleFacebookAccessToken(AccessToken accessToken) {
+
+        Log.d("TAG", "handleFacebookAccessToken: " + accessToken);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d("TAG", "signInWithCredential:onComplete:" + task.isSuccessful());
+
+
+
+                if (!task.isSuccessful()) {
+                    Log.w("TAG", "signInWithCredential", task.getException());
+                    Toast.makeText(MainActivity.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthStateListener != null) {
+            mAuth.removeAuthStateListener(mAuthStateListener);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
